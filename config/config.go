60426d5510a13a1cedb4f7295b398a9064166620
liuxu <928525166@qq.com>
2021-01-06 17:02:28 +0800

    1.配置node的节点信息从config配置文件读取 2.调整Node结构体的位置

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
@@ -17,6 +18,14 @@ type Server struct {
 	Domain string `json:"domain"`
 	Port   int    `json:"port"`
 	Name   string `json:"name"`
+	Nodes  []Node `json:"nodes"`
+}
+
+type Node struct {
+	Id   string `json:"id"`
+	Ip   string `json:"ip"`
+	Port int    `json:"port"`
+	Term int    `json:"term"`
 }
 
 var Conf Config
@@ -34,4 +43,7 @@ func init() {
 		fmt.Println("Parse the config file is error!")
 	}
 	err = viper.Unmarshal(&Conf)
+
+	jsonString, _ := json.Marshal(&Conf)
+	fmt.Printf("查看加载的config配置信息:%s", string(jsonString))
 }
