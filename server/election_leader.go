0c3a08c2b63120058c1224c70e9707daee933bd1
liuxu <928525166@qq.com>
2021-01-06 16:41:05 +0800

    采用gorountie的方式来执行这个trigger

--- server/election_leader.go
+++ server/election_leader.go
@@ -1,5 +1,23 @@
 package server
 
+import (
+	"fmt"
+	"time"
+)
+
 //
 // election leader:leader的选举
 //
+
+func triggerElection() {
+	duration := time.Second * 2
+	timer := time.NewTimer(duration)
+	defer timer.Stop()
+
+	<-timer.C
+	fmt.Println("定时器爆炸，开始启动election....")
+}
+
+func init() {
+	go triggerElection()
+}
