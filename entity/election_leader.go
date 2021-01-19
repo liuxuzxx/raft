612735daf045d441febc2b8129b00185a72c3d1b
liuxu <928525166@qq.com>
2021-01-19 15:44:23 +0800

    增加发送权威的结构体

--- entity/election_leader.go
+++ entity/election_leader.go
@@ -22,3 +22,12 @@ type VoteResponse struct {
 	Result   VoteResult `json:"result"`
 	Describe string     `json:"describe"`
 }
+
+//
+//当leader选举出来之后，需要周期的发送心跳给其他
+//的没有在这个Term当选的node，来建立权威
+//
+type LeaderAuthority struct {
+	LeaderId string
+	Term     int
+}
